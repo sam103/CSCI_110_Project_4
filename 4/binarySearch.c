783be6a69584919ef7a49b1d71fb5bce397b98d5
sam103 <telecasterbeck@yahoo.com>
2012-06-05 18:18:02 -0700

    finished, just needs the quicksort file, already tested it with different txt files.

--- 4/binarySearch.c
+++ 4/binarySearch.c
@@ -1,26 +1,60 @@
+/*******************************************************************************
+ CSCI 110 MW 
+ project 4 question 4 
+ by      Mao Samuel 
+             Jonnhaou Sean Huang
+             Kevin Gonzales
+             Jonathan Hernandez
+ ******************************************************************************/
+
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
-main()  {
-        int
-           arr[]= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20},
-           count;
+main() {
+int arr[200],
+    a,c,count;
+
+FILE
+
+      *inptr;
+   
+   a=0;
+   
+   
+   inptr = fopen ("quickSortOutput", "r"); /* read txt file from equestion 1 */
+   c = fscanf(inptr,"%d",&arr);
+   
+   while (c != EOF)
+   {
+           a=a+1;
+	   c = fscanf(inptr,"%d",&arr[a]); /* crates an array from  the file*/
+   }
+       
         count = sizeof(arr);
-        printf("number of elements in the array: %d\n",count/sizeof(int));
         
-        int
-           DESIRED_VAL,
-           i,j,
-           lower,
-           upper,
-           middle;
-        printf("please enter desired value\n");
-        scanf("%d",&DESIRED_VAL); 
+       
+   int
+      DESIRED_VAL,
+      i,j,k,
+      lower,
+      upper,
+      middle;
+           
+           
+        printf("please enter desired value\n"); /* allows user to define the key*/
+        scanf("%d",&DESIRED_VAL);
         printf("the sequence of numbers start at position 0\n");
+        
+        
         {
+        count = sizeof(arr);          /* counts the number of elements in array*/
         for (i = 0; i < (count/sizeof(int)); i=i+1)
         
-        lower = 0;
+       
+        lower = 0;  
         upper = (count/sizeof(int)) - 1;
         middle = (upper - lower) / 2;
         while((arr[middle] != DESIRED_VAL) && (lower <= upper))
@@ -35,15 +69,15 @@ main()  {
                  upper = middle - 1;
                  middle = (upper - lower) / 2 + lower;
              }
-       }            
+       }
        if (arr[middle] == DESIRED_VAL)
           printf("desired value %d found in position %d\n",DESIRED_VAL,middle);
          
-          else            
+          else
               printf("desired value %d not in array\n",DESIRED_VAL );
-              system("pause");   /* command to make my execution screen from exiting*/
-              return(0);      
-}  
-} 
+              system("pause"); /* command to make my execution screen from exiting*/
+              return(0);
+}
+}
       
 
